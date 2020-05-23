c44ecb0f6bbd1ff4f1e5b9904bf5d606e4c9266a
wangxiaozhan <w2457307263@outlook.com>
2020-05-23 19:02:48 +0800

    react开发环境配置

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: {
+    index: './src/index.js'
+  },
+  module: {
+    rules: [
+        {
+            test:/\.css$/,
+            use:['style-loader','css-loader']
+        },
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
+        }
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin()
+  ],
+  output: {
+    filename: '[name].[hash:5].bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  devtool: 'inline-source-map',
+  devServer: {
+    contentBase: './dist'
+  }
+}
